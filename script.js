5dba6c7e4de3a697bd90a77b7d392b124a829ccf
root <root@DesktopRobertt>
2023-12-01 17:15:34 -0300

    version mobile

---
+++ script.js
@@ -0,0 +1,72 @@
+document.getElementById("calculateButton").addEventListener("click", function() {
+    calculateBitcoinValue();
+});
+
+function calculateBitcoinValue() {
+    const currency = document.getElementById("currency").value;
+    const amount = parseFloat(document.getElementById("amount").value);
+    const date = document.getElementById("date").value;
+
+    if (isNaN(amount) || amount <= 0) {
+        alert("Por favor, insira uma quantidade válida na moeda selecionada.");
+        return;
+    }
+
+    const currencySymbol = (currency === "USD") ? "$" : "R$";
+
+    $.ajax({
+        url: `https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=${currency}&ts=${Date.parse(date) / 1000}`,
+        success: function(data) {
+            if (data.BTC && data.BTC[currency]) {
+                const bitcoinPriceAtPurchase = data.BTC[currency];
+                const amountInBTC = amount / bitcoinPriceAtPurchase;
+
+                // Obtenha o preço atual do Bitcoin
+                $.ajax({
+                    url: `https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=${currency}`,
+                    success: function(currentData) {
+                        if (currentData[currency]) {
+                            const currentBitcoinPrice = currentData[currency];
+                            const totalValueNow = amountInBTC * currentBitcoinPrice;
+                            const profitInCurrency = totalValueNow - amount;
+                            const profitPercentage = (profitInCurrency / amount) * 100;
+
+                            const textResultHTML = `<h2>Você agora teria <span class="textdecorResult">${amountInBTC.toFixed(3)} BTC</span>, que valem <span class="textdecorResult">${currency} ${totalValueNow.toFixed(2)}</span> hoje.</h2>
+                            <h2>Seria um lucro de <span class="textdecorResult2">${profitPercentage.toFixed(2)}%</span></h2>`
+                            document.querySelector(".resultText").innerHTML = textResultHTML;
+                            // Cards informativos
+                            const resultHTML = `
+                            <div class="card-item"><p>Preço de Compra (na data informada) </p><span id="purchasePrice">${currencySymbol}${bitcoinPriceAtPurchase.toFixed(2)}</span></div>
+                            <div class="card-item"><p>Quantidade (BTC que poderia ter comprado na data informada) </p><span id="purchaseAmount">${amountInBTC.toFixed(6)} BTC</span></div>
+                            <div class="card-item"><p>Preço Atual do Bitcoin </p><span id="currentPrice">${currencySymbol}${currentBitcoinPrice.toFixed(2)}</span></div>
+                            <div class="card-item"><p>Valor Total Agora </p><span id="currentTotalValue">${currencySymbol}${totalValueNow.toFixed(2)}</span></div>
+                            <div class="card-item"><p>Lucro em ${currency} </p><span id="profitInUSD">${currencySymbol}${profitInCurrency.toFixed(2)}</span></div>
+                            <div class="card-item"><p>Porcentagem de Ganho ou Perda </p><span id="profitPercentage">${profitPercentage.toFixed(2)}%</span></div>
+                                
+                            `;
+
+                            document.getElementById("result").innerHTML = resultHTML;
+                        } else {
+                            alert("Não foi possível obter o preço atual do Bitcoin.");
+                        }
+                    },
+                    error: function() {
+                        alert("Ocorreu um erro ao buscar o preço atual do Bitcoin.");
+                    }
+                });
+            } else {
+                alert("Não foi possível obter os dados de preço na data informada.");
+            }
+        },
+        error: function() {
+            alert("Ocorreu um erro ao buscar os dados de preço do Bitcoin na data informada.");
+        }
+    });
+}
+
+
+
+
+  var typed = new Typed('#element', {
+    strings: ['<h2>Bitcoin Time Machine.</h2> <br>^1000\n <p>`Veja quanto você teria se tivesse comprado Bitcoin quando você conheceu a cripto`</p>'],
+  });
