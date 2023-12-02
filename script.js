f27f6774b418685d9b768cae99b1e740bea9695a
root <root@DesktopRobertt>
2023-12-02 01:31:14 -0300

    add grafico de historico de preco do btc

--- script.js
+++ script.js
@@ -83,3 +83,142 @@ function calculateBitcoinValue() {
         }
     });
 }
+
+
+
+
+
+
+
+
+const apiUrl = 'https://api.coingecko.com/api/v3/';
+    const chartElement = document.getElementById('chart');
+    let chartData = [];
+
+    function getData(range) {
+      let url;
+      const currentDate = new Date().toISOString().split('T')[0];
+
+      switch (range) {
+        case '1w':
+          url = `${apiUrl}coins/bitcoin/market_chart?vs_currency=brl&days=7&interval=daily`;
+          break;
+        case '1m':
+          url = `${apiUrl}coins/bitcoin/market_chart?vs_currency=brl&days=30&interval=daily`;
+          break;
+        case '6m':
+          url = `${apiUrl}coins/bitcoin/market_chart?vs_currency=brl&days=180&interval=daily`;
+          break;
+        case '1y':
+          url = `${apiUrl}coins/bitcoin/market_chart?vs_currency=brl&days=365&interval=daily`;
+          break;
+        case 'max':
+          url = `${apiUrl}coins/bitcoin/market_chart?vs_currency=brl&days=max&interval=daily`;
+          break;
+      }
+
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          chartData = data.prices;
+          plotChart(chartData);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    }
+
+    function plotChart(prices) {
+      const formattedData = prices.map(entry => ({
+        x: new Date(entry[0]),
+        y: entry[1]
+      }));
+
+      const options = {
+        series: [{
+          name: 'Bitcoin Price (BRL)',
+          data: formattedData
+        }],
+        chart: {
+          type: 'area',
+          height: 350,
+          toolbar: {
+            show: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        xaxis: {
+          type: 'datetime'
+        },
+        yaxis: {
+          labels: {
+            formatter: function (value) {
+              return 'R$ ' + value.toFixed(2);
+            }
+          }
+        },
+        title: {
+          text: 'Gráfico de preços do Bitcoin',
+          align: 'center'
+        },
+        noData: {
+          text: 'No data available',
+          align: 'center',
+          verticalAlign: 'middle',
+          offsetX: 0,
+          offsetY: 0,
+          style: {
+            color: '#FF0000',
+            fontSize: '20px'
+          }
+        },
+        tooltip: {
+          x: {
+            format: 'dd/MM/yyyy HH:mm'
+          }
+        },
+        locales: [{
+          name: 'pt-br',
+          options: {
+            months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+            shortMonths: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+            days: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+            shortDays: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+            toolbar: {
+              exportToSVG: 'Exportar SVG',
+              exportToPNG: 'Exportar PNG',
+              menu: 'Menu',
+              selection: 'Selecionar',
+              selectionZoom: 'Selecionar Zoom',
+              zoomIn: 'Zoom In',
+              zoomOut: 'Zoom Out',
+              pan: 'Pan',
+              reset: 'Resetar Zoom'
+            }
+          }
+        }],
+        defaultLocale: 'pt-br'
+      };
+
+      const chart = new ApexCharts(chartElement, options);
+      chart.render();
+    }
+
+    function downloadCSV() {
+      const csvContent = "data:text/csv;charset=utf-8," +
+        "Date,Price\n" +
+        chartData.map(entry => `${new Date(entry[0]).toLocaleString()},${entry[1]}`).join("\n");
+
+      const encodedUri = encodeURI(csvContent);
+      const link = document.createElement("a");
+      link.setAttribute("href", encodedUri);
+      link.setAttribute("download", "bitcoin_prices.csv");
+      document.body.appendChild(link); // Required for Firefox
+      link.click();
+    }
+
+    // Initialize with default data (1 week)
+    getData('1w');
